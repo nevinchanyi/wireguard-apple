1c043f52079f6914204806eba05b8986eddab0db
Kostiantyn Nevinchanyi <59333838+nevinchanyi@users.noreply.github.com>
2025-03-14 20:43:47 -0500

    Update WireGuardKitC.h
    Signed-off-by: Kostiantyn Nevinchanyi <59333838+nevinchanyi@users.noreply.github.com>

--- Sources/WireGuardKitC/WireGuardKitC.h
+++ Sources/WireGuardKitC/WireGuardKitC.h
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 // Copyright © 2018-2023 WireGuard LLC. All Rights Reserved.
-
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/kern_control.h>
 #include "key.h"
 #include "x25519.h"
 
